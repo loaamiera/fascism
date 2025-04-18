40a2af58d189832644b63ea5db39829b9a5fd170
loaamiera <brownamiera@gmail.com>
2025-04-18 00:01:24 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+var altmessage = document.getElementById("censor");
+
+var picture = document.getElementById("stages");
+
+altmessage.onclick = function Censors() {
+  altmessage.innerHTML = "The Application of Protecting Americans from Foreign Adversary Controlled Applications Act to TikTok is meant to prohibit TikTok use in America until June 19th. They state that the idea is to resolve this issue with them. Lots, including Keene State College, point out that a characteristic of fascism is the control of mass media and obsession with national security.";
+
+} ;
+
+
+altmessage.onmouseover = function warning() {
+  altmessage.innerHTML = "But many believe that the Tiktok ban is really due to the political content and news spread throughout the app." ;
+} ;
+
+
+picture.onclick = function trump() {
+  picture.src = 'stages.png' ;
+} ;
+
+
+
+
+
+
+
